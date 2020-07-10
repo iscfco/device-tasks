326fade2573baa8a95db7c8be2ce375bb369ca37
Francisco <francisco@ivedha.com>
2020-07-09 23:33:57 -0500

    Scenarios reader (a funtion to read the scenarios from a file) created and implemented

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
 func main() {
 	// Read scenarios:
+	scenarios, err := ReadScenarios()
+	if err != nil {
+		log.Fatal("Error at moment of read file", err)
+	}
+	fmt.Println(*scenarios)
 
 	// Scenarios Parser:
 	// - Will read data and determine "Resource Capacity", "Background Tasks" and "Foreground Tasks"
@@ -9,4 +20,15 @@ func main() {
 	// Getting optimal configuration:
 
 	// Writing result:
+}
+
+// ReadScenarios will read the possible scenarios from a file, we assume the file is
+// located in the same level of this file
+func ReadScenarios() (*string, error) {
+	file, err := ioutil.ReadFile("challenge.in")
+	if err != nil {
+		return nil, err
+	}
+	data := string(file)
+	return &data, nil
 }
