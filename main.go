85d1f1d60fd8637acb6e2aa46257afbd963b7e26
Francisco <francisco@ivedha.com>
2020-07-09 23:14:35 -0500

    Wrting (as comments) the steps to follow

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+func main() {
+	// Read scenarios:
+
+	// Scenarios Parser:
+	// - Will read data and determine "Resource Capacity", "Background Tasks" and "Foreground Tasks"
+
+	// Getting optimal configuration:
+
+	// Writing result:
+}
